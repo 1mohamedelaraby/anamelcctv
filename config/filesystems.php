d80ff3a31fc1611f5c2740ff4f6ae80199bde747
Mohamed Elaraby <32622984+1mohamedelaraby@users.noreply.github.com>
2021-10-30 16:56:31 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,6 +54,13 @@ return [
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
+        
+         'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public/img/'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+        ],
 
         's3' => [
             'driver' => 's3',
